49041a9710828c1787060d3ff94e0b2bc82ce47c
Erik Badger <erikbadger777@gmail.com>
2020-05-10 18:24:26 -0500

    made embeded video responsive

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,13 @@ module.exports = {
                                 // The service-specific options by the name of the service
                             },
                         },
-                    }
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                        },
+                    },
                 ],
             },
         },
