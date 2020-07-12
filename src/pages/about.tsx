ac1bd6b0b20ae5ad63be941ea2e9be3a458a4b86
Erik Badger <erikbadger777@gmail.com>
2020-07-12 17:06:27 -0500

    added about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import Layout from '../components/layout';
+
+interface AboutPageProps {
+}
+
+const AboutPage:FC<AboutPageProps> = ({}) => {
+    return (
+        <Layout>
+            <div>AboutPage works!</div>
+        </Layout>
+    );
+};
+
+export default AboutPage;
