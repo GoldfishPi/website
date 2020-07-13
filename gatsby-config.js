bdf200a266e46894a13ab2f9912f41cb525f60b9
Erik Badger <erikbadger777@gmail.com>
2020-07-12 19:48:32 -0500

    removed blog content so gatsby would compile

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,8 +38,32 @@ module.exports = {
                     {
                         resolve: `gatsby-remark-images`,
                         options: {
-                            maxWidth: 1035,
-                            sizeByPixelDensity: true,
+                            // It's important to specify the maxWidth (in pixels) of
+                            // the content container as this plugin uses this as the
+                            // base for generating different widths of each image.
+                            maxWidth: 590,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-embed-video`,
+                        options: {
+                            width: 800,
+                            ratio: 1.77, 
+                            height: 400, 
+                            related: false,
+                            noIframeBorder: true,
+                            customTransformers: [
+                                // Your custom transformers
+                            ],
+                            services: {
+                                // The service-specific options by the name of the service
+                            },
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
                         },
                     },
                 ]
