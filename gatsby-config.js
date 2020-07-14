62fbc41d0b012acfeec0669f5a65852e021d6c46
Erik Badger <erikbadger777@gmail.com>
2020-07-14 17:46:12 -0500

    added robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -149,6 +149,7 @@ module.exports = {
                 fonts: [`Roboto`],
             },
         },
+        `gatsby-plugin-robots-txt`,
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
