0fce6d5ac0c29cbd598d4360c637f4b2e4e528d2
Erik Badger <erikbadger777@gmail.com>
2020-11-30 23:07:44 -0600

    (feature) added blog cards section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,118 @@
-import React from "react"
-import { Link, graphql, PageProps } from "gatsby"
-import styled from "styled-components"
-import { Card } from '@lassiebug/card';
+import {graphql, PageProps} from "gatsby";
+import React from "react";
+import styled from "styled-components";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import LoremIpsum from 'react-lorem-ipsum';
+import {Card} from "@lassiebug/card";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+
+const IndexPage:React.FC<PageProps<any>> = ({ data }) => {
+    const banner:string = data.banner.childImageSharp.fluid.src;
+    return (
+        <Layout>
+            <SEO title="Home | Blog Posts"/>
+            <ContentStyles>
+                <div id="header">
+                    <img className="__bg-img" src={banner}/>
+                    <div className="__bg-text">
+                        <h1>Erik Badger</h1>
+                        <h2>Simple Web Developer</h2>
+                        <a href="#banner" className="__start-btn">Start</a>
+                    </div>
+                </div>
+                <div id="banner">
+                    <BannerStyles>
+                        <div>
+                            <div className="__heading">
+                                <h2>lorem ipsum</h2>
+                                <h3>dolar gammet takem</h3>
+                            </div>
+                            <p className="__about">
+                                <LoremIpsum
+                                    avgWordsPerSentence={4}
+                                    avgSentencesPerParagraph={4}
+                                />
+                            </p>
+                            <div className="__cards">
+                                <Card header={() => <p>lorem</p>} img={banner}>
+                                    <p>
+                                        <LoremIpsum
+                                            avgWordsPerSentence={4}
+                                            avgSentencesPerParagraph={4}
+                                        />
+                                    </p>
+                                </Card>
+                                <Card header={() => <p>lorem</p>} img={banner}>
+                                    <p>
+                                        <LoremIpsum
+                                            avgWordsPerSentence={4}
+                                            avgSentencesPerParagraph={4}
+                                        />
+                                    </p>
+                                </Card>
+                                <Card header={() => <p>lorem</p>} img={banner}>
+                                    <p>
+                                        <LoremIpsum
+                                            avgWordsPerSentence={4}
+                                            avgSentencesPerParagraph={4}
+                                        />
+                                    </p>
+                                </Card>
+
+                            </div>
+                        </div>
+                        <a href="#section" className="__start-btn">^</a>
+                    </BannerStyles>
+                </div>
+                <div id="section">
+                </div>
+            </ContentStyles>
+        </Layout>
+    )
+}
+
+const BannerStyles = styled.div`
+
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    flex-direction:column;
+
+    flex-grow:1;
+
+    .__heading {
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        flex-direction:column;
+    }
+    h2 {
+        margin-bottom:var(--padding-05);
+    }
+    h3 {
+        margin-bottom:var(--padding-05);
+    }
+    .__about {
+        padding:var(--padding-05);
+        padding-top:0;
+    }
+    .__cards {
+        display:grid;
+        gap:var(--padding-05);
+        grid-template-columns:repeat(auto-fit, minmax(20rem, 1fr));
+        padding:var(--padding-05);
+    }
+`;
 
 const ContentStyles = styled.div`
     width:100%;
     scroll-behavior:smooth;
 
     & > div {
-        height:100vh;
+        min-height:100vh;
+        display:flex;
+        flex-direction:column;
     }
 
     #header {
@@ -69,34 +170,12 @@ const ContentStyles = styled.div`
     }
 
     @media screen and (min-width:1400px) {
-        .__banner {
+        .__bg-img {
             width:100vw;
             height:auto;
         }
     }
 `;
-
-const IndexPage:React.FC<PageProps<any>> = ({ data }) => {
-    console.log('data', data);
-    return (
-        <Layout>
-            <SEO title="Home | Blog Posts"/>
-            <ContentStyles>
-                <div id="header">
-                    <img className="__bg-img" src={data.banner.childImageSharp.fluid.src}/>
-                    <div className="__bg-text">
-                        <h1>Erik Badger</h1>
-                        <h2>Simple Web Developer</h2>
-                        <a href="#banner" className="__start-btn">Start</a>
-                    </div>
-                </div>
-                <div id="banner">
-                    <p>Hello</p>
-                </div>
-            </ContentStyles>
-        </Layout>
-    )
-}
 
 export const query = graphql`
         {
