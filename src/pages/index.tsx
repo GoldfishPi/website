c5e92ebfd534531ed45e31c615cdc751bfbcb492
Erik Badger <erikbadger777@gmail.com>
2020-11-30 22:32:14 -0600

    (refactor) re-doing site styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,42 +8,90 @@ import SEO from "../components/seo"
 
 const ContentStyles = styled.div`
     width:100%;
-    display: grid;
-    grid-template-columns: 1fr;
+    scroll-behavior:smooth;
 
-    .__cards {
-        width: 100%;
+    & > div {
+        height:100vh;
     }
 
-    @media screen and (min-width:900px) {
-        grid-template-columns:1fr 3fr 1fr;
-        .__cards {
-            grid-column:2;
+    #header {
+        position:relative;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        max-width:100vw;
+        overflow:hidden;
+    }
+
+    .__bg-img {
+        height:100vh;
+        filter:blur(8px);
+    }
+
+    .__bg-text {
+        position:absolute;
+        text-align:center;
+
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+    }
+
+    h1 {
+        padding-bottom:var(--padding-05);
+        border-bottom:2px solid var(--primary);
+    }
+
+    h2 {
+        padding-top:var(--padding-05);
+    }
+
+    .__start-btn {
+        width:100px;
+        height:100px;
+
+        background-color:var(--primary);
+        color:var(--text);
+        border:none;
+
+        border-radius:100%;
+        font-size:1.75rem;
+
+        margin-top:1rem;
+
+        text-decoration:none;
+        font-weight:bold;
+        cursor: pointer;
+
+        display:flex;
+        justify-content:center;
+        align-items:center;
+    }
+
+    @media screen and (min-width:1400px) {
+        .__banner {
+            width:100vw;
+            height:auto;
         }
     }
 `;
 
 const IndexPage:React.FC<PageProps<any>> = ({ data }) => {
+    console.log('data', data);
     return (
         <Layout>
             <SEO title="Home | Blog Posts"/>
             <ContentStyles>
-                <div className="__cards">
-                    <h1>Blog Posts</h1>
-                    <br/>
-                    {data.allPages.edges.map((page:any) => (                
-                        <Link 
-                            to={`blog${page.node.markdown.frontmatter.path}`}
-                            key={page.node.markdown.frontmatter.path}
-                        >
-                            <Card
-                                hoverable
-                            >
-                                <h2>{ page.node.markdown.frontmatter.title }</h2>
-                                <p>{page.node.markdown.excerpt}</p>
-                            </Card>
-                        </Link>
-                    ))}
+                <div id="header">
+                    <img className="__bg-img" src={data.banner.childImageSharp.fluid.src}/>
+                    <div className="__bg-text">
+                        <h1>Erik Badger</h1>
+                        <h2>Simple Web Developer</h2>
+                        <a href="#banner" className="__start-btn">Start</a>
+                    </div>
+                </div>
+                <div id="banner">
+                    <p>Hello</p>
                 </div>
             </ContentStyles>
         </Layout>
@@ -65,6 +113,13 @@ export const query = graphql`
                                 path
                             }
                         }
+                    }
+                }
+            }
+            banner:file(relativePath:{eq:"banner.jpg"}) {
+                childImageSharp {
+                    fluid(maxWidth:1920, maxHeight:1080) {
+                        src
                     }
                 }
             }
